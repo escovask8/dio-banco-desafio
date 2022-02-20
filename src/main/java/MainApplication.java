34a9f40646a5c93fec1180c9dfb2766df163c454
Victor Dutra <domagal@gmail.com>
2022-02-20 16:53:27 -0300

    Subindo projeto Dio

---
+++ src/main/java/MainApplication.java
@@ -0,0 +1,16 @@
+public class MainApplication {
+    public static void main(String[] args) {
+        Cliente antonio = new Cliente();
+		antonio.setNome("Antonio");
+		
+		Conta cc = new ContaCorrente(antonio);
+		Conta poupanca = new ContaPoupanca(antonio);
+
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+
+    }
+}
